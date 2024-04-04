ee99fc2caff9979e3e94c8699156171222162e39
Yennyth Sánchez <yennyth.es@gmail.com>
2024-04-04 16:54:56 +0100

    - EventCard > Botón > design

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,46 +1,63 @@
 import React from 'react';
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import { CalendarMonth, Groups, LocationOn } from '@mui/icons-material'
 
 const EventCard = (
-    // { title, date, description, address, registered, imageUrl }
-    ) => {
+    // { title, date, info, address, registered, imageUrl }
+) => {
     const title = "titulo"
     const date = "date"
-    const description = "description"
+    const info = "info"
     const address = "address"
-    const participants = "resgistered"
+    const inscribed = "20"
+    const participants = "50"
     const imageUrl = "img"
     return (
         <Box>
-            <Card sx={{ width:'250px', height:'350px' }}>
+            <Card sx={{ width: '250px', height: '350px' }} elevation={4}>
                 <CardMedia
                     component="img"
-                    border= '1px'
+                    border='1px'
                     height="150"
                     image={imageUrl}
                     alt={title}
                 />
+                <CardContent sx={{padding:'20px'}}>
 
-                <CardContent>
-
-                    <Typography gutterBottom variant="h5" /* component="div" */ sx={{ textAlign: 'center'}}>
+                    <Typography gutterBottom variant="h5" /* component="div" */ sx={{ textAlign: 'center' }}>
                         {title}
                     </Typography>
-                    <Typography variant="body2" color="text.main">
-                        {date}
-                    </Typography>
-                    <Typography variant="body2" color="text.main">
-                        {description}
-                    </Typography>
-                    <Typography variant="body2" color="text.main">
-                        {participants}
-                    </Typography>
-                    <Typography variant="body2" color="text.main">
-                        {address}
-                    </Typography>
-                </CardContent>
-            </Card>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+                        <CalendarMonth /> {/* Icono de calendario */}
+                        <Typography variant="body2" color="text.main">
+                            {date}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
 
+                            <LocationOn />
+                            <Typography variant="body2" color="text.main">
+                                {address}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+                            <Groups />
+                            <Typography variant="body2" color="text.main">
+                                {inscribed + ' / ' + participants}
+                            </Typography>
+                        </Box>
+                    </Box>
+                </CardContent>
+                <CardActions sx={{justifyContent:'center'}}>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 'auto' }}>
+                        <Button variant="contained" color="success">
+                            {info}
+                        </Button>
+                    </Box>
+                </CardActions>
+            </Card>
         </Box>
 
 
