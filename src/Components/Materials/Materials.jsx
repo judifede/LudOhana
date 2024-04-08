7168cbd46280a8d7327fe9d409ea59b039cdbd97
bqcount <bqcount@gmail.com>
2024-04-08 20:42:44 +0100

    render data of materials

--- src/Components/Materials/Materials.jsx
+++ src/Components/Materials/Materials.jsx
@@ -1,46 +1,34 @@
-
-import { Typography, Grid,TableRow,TableCell,Button } from '@mui/material'
+/* 
+import { Typography, Grid,TableRow,TableCell,Button, TableBody, TableContainer, Table, TableHead } from '@mui/material'
 import { getMaterialsEvents } from '../../Services/materials';
 import { useState } from 'react'
 
-const columns = [
-  {
-    width: 200,
-    label: 'Dessert',
-    dataKey: 'dessert',
-  },
-  {
-    width: 120,
-    label: 'Calories\u00A0(g)',
-    dataKey: 'calories',
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: 'Fat\u00A0(g)',
-    dataKey: 'fat',
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: 'Carbs\u00A0(g)',
-    dataKey: 'carbs',
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: 'Protein\u00A0(g)',
-    dataKey: 'protein',
-    numeric: true,
-  },
-];
 
 function Materials() {
   const[materialsEvents,setMaterialsEvents]=useState([])
-  const handleMaterialsEvents = async ()=>{
-
-    await setMaterialsEvents(getMaterialsEvents())
+  const [materials,setMaterials]=useState([])
+  const [events,setEvents]=useState([])
+  const handleMaterialsEvents = async () => {
+    const data = await getMaterialsEvents();
+    setMaterialsEvents(data.allMaterialEvent);
   }
+  const handleMaterials = async()=>{
+    const data = await getMaterialsEvents()
+    setMaterials(data.allMaterial)
+  }
+  const handleEvents = async()=>{
+    const data = await getMaterialsEvents()
+    setEvents(data.allEvent)
+  }
+  const columns = [
+    { id: 'id', label: 'ID del Material' },
+    { id: 'name', label: 'Nombre del Material' },
+    { id: 'amount', label: 'Cantidad' },
+    { id: 'amountUsed', label: 'En Uso' },
+    { id: 'eventId', label: 'ID del Evento' },
+    { id: 'eventTitle', label: 'Nombre del Evento' },
+    { id: 'actions', label: 'Acciones' }, 
+  ];
 
   return (
     
@@ -59,22 +47,173 @@ function Materials() {
         Gestor de Materiales 
       </Typography>
 
-      <TableRow>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          variant="head"
-          align={column.numeric || false ? 'right' : 'left'}
-          style={{ width: column.width }}
-          sx={{
-            backgroundColor: 'background.paper',
-          }}
-        >
-          {column.label}
-        </TableCell>
-      ))}
-    </TableRow>    
-    <Button onClick={()=>{handleMaterialsEvents()}}>Prueba </Button>     
+      <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id} align="center">
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {materialsEvents.map((row) => {
+                const material = materials.find((m) => m.id === row.materialId); // Encontrar el material correspondiente
+                const event = events.find((e)=>e.id === row.eventId)
+                return (
+                  <TableRow key={row.materialId}>
+                    <TableCell align="center">{material.id}</TableCell>
+                    <TableCell align="center">{material.name}</TableCell>
+                    <TableCell align="center">{material.amount}</TableCell>
+                    <TableCell align="center">{row.amountUsed}</TableCell>
+                    <TableCell align="center">{event.id}</TableCell>
+                    <TableCell align="center">{event.title}</TableCell>
+                    <TableCell align="center">
+                      <Button variant="outlined" color="primary" style={{ marginRight: '8px' }}>
+                        Editar
+                      </Button>
+                      <Button variant="outlined" color="secondary">
+                        Borrar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    <Button onClick={()=>{handleMaterialsEvents(); handleMaterials() ;handleEvents()}}>Prueba </Button>     
+    </Grid>
+  )
+}
+
+export default Materials
+ */
+
+import { useState, useEffect } from 'react'
+import {
+  Typography,
+  Grid,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableContainer,
+  Table,
+  TableHead,
+  InputAdornment,
+  Icon,
+} from '@mui/material'
+import { getMaterialsEvents } from '../../Services/materials'
+import { Delete, Edit } from '@mui/icons-material'
+
+function Materials() {
+  const [materialsEvents, setMaterialsEvents] = useState([])
+  const [materials, setMaterials] = useState([])
+  const [events, setEvents] = useState([])
+
+  const columns = [
+    { id: 'id', label: 'ID del Material' },
+    { id: 'name', label: 'Nombre del Material' },
+    { id: 'amount', label: 'Cantidad' },
+    { id: 'amountUsed', label: 'En Uso' },
+    { id: 'eventId', label: 'ID del Evento' },
+    { id: 'eventTitle', label: 'Nombre del Evento' },
+    { id: 'actions', label: 'Acciones' },
+  ]
+
+  const fetchData = async () => {
+    try {
+      const [materialsData, materialsEventsData, eventsData] =
+        await Promise.all([
+          getMaterialsEvents(),
+          getMaterialsEvents(),
+          getMaterialsEvents(),
+        ])
+      setMaterials(materialsData.allMaterial)
+      setMaterialsEvents(materialsEventsData.allMaterialEvent)
+      setEvents(eventsData.allEvent)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12}>
+        <Typography variant="h3" component="h2" align="center" gutterBottom>
+          Gestor de Materiales
+        </Typography>
+
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id} align="center">
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {materialsEvents.map((row) => {
+                const material = materials.find((m) => m.id === row.materialId)
+                const event = events.find((e) => e.id === row.eventId)
+
+                return (
+                  <TableRow key={row.materialId}>
+                    <TableCell align="center">
+                      {material ? material.id : 'No disponible'}
+                    </TableCell>
+                    <TableCell align="center">
+                      {material ? material.name : 'No disponible'}
+                    </TableCell>
+                    <TableCell align="center">
+                      {material ? material.amount : 'No disponible'}
+                    </TableCell>
+                    <TableCell align="center">{row.amountUsed}</TableCell>
+                    <TableCell align="center">
+                      {event ? event.id : 'No disponible'}
+                    </TableCell>
+                    <TableCell align="center">
+                      {event ? event.title : 'No disponible'}
+                    </TableCell>
+                    <TableCell 
+                    sx={{
+                      display:"flex",
+                      flexDirection:"row", 
+                      justifyContent:"space-between",
+                      alignItems:"center",
+                  
+                      }}>
+                       <InputAdornment >
+                         <Icon>
+                           <Edit />
+                         </Icon>
+                         <Icon>
+                           
+                           <Delete/>
+                         </Icon>
+                       </InputAdornment>
+                       {/* <InputAdornment>
+                         <Icon>
+                           <Delete/>
+                         </Icon>
+                       </InputAdornment> */}
+                     
+                    
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
     </Grid>
   )
 }
--- src/Services/materials.js
+++ src/Services/materials.js
@@ -3,8 +3,8 @@ import api from './config'
 
 export const getMaterialsEvents= async () => {
     const response = await api.get('/api/events/materials',{headers:{Authorization:localStorage.getItem('token')}})
-    console.log(response)
-    return response
+    console.log(response.data.allMaterialEventData)
+    return response.data.allMaterialEventData
 }
 
 
