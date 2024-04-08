de1c554155725f683bfd1b81cbc68a86bb65f106
Daniela Romero Garcia <bqcount@gmail.com>
2024-04-08 16:54:28 +0100

    fixed getAlls

--- src/Services/materials.js
+++ src/Services/materials.js
@@ -1,7 +1,8 @@
+
 import api from './config'
 
 export const getMaterialsEvents= async () => {
-    const response = await api.get('/api/events/materials')
+    const response = await api.get('/api/events/materials',{headers:{Authorization:localStorage.getItem('token')}})
     console.log(response)
     return response
 }
